129b26a6a72c8f6de5557ec49ff31760204aa181
Valera Trubachev <vetruvet@gmail.com>
2014-04-27 21:06:35 -0500

    Initial commit.

---
+++ src/Vetruvet/PhpRedis/Database.php
@@ -0,0 +1,112 @@
+<?php 
+
+namespace Vetruvet\PhpRedis;
+
+class Database extends Illuminate\Redis\Database {
+
+    /**
+     * Create a new aggregate client supporting sharding.
+     *
+     * @param  array  $servers
+     * @return array
+     */
+    protected function createAggregateClient(array $servers) {
+        $options = array(
+        	'lazy_connect' => true,
+	        'pconnect'     => false,
+	        'timeout'      => 0,
+	    );
+
+        $cluster = array();
+        foreach ($servers as $key => $server) {
+        	if ($key === 'cluster') continue;
+
+        	$host    = empty($server['host'])    ? '127.0.0.1' : $server['host'];
+            $port    = empty($server['port'])    ? '6379'      : $server['port'];
+
+            $serializer = Redis::SERIALIZER_NONE;
+            if (!empty($server['serializer'])) {
+            	if ($server['serializer'] === 'none') {
+            		$serializer = Redis::SERIALIZER_PHP;
+            	} else if ($server['serializer'] === 'igbinary' && defined('Redis::SERIALIZER_IGBINARY')) {
+            		$serializer = Redis::SERIALIZER_IGBINARY;
+            	}
+            }
+
+            $cluster[$host.':'.$port] = array(
+            	'prefix'     => empty($server['prefix'])   ? '' : $server['prefix'],
+            	'database'   => empty($server['database']) ? 0  : $server['database'],
+            	'serializer' => $serializer,
+        	);
+
+            if (isset($server['persistent'])) {
+            	$options['pconnect'] = $options['pconnect'] && $server['persistent'];
+            } else {
+				$options['pconnect'] = false;
+            }
+
+            if (!empty($server['timeout'])) {
+            	$options['timeout'] = max($options['timeout'], $server['timeout']);
+            }
+        }
+
+        $ra = new RedisArray(array_keys($cluster), $options);
+
+        foreach ($cluster as $host => $options) {
+        	$redis = $ra->_instance($host);
+        	$redis->setOption(Redis::OPT_PREFIX, $options['prefix']);
+        	$redis->setOption(Redis::OPT_SERIALIZER, $options['serializer']);
+        	$redis->select($options['database']);
+        }
+
+        return array('default' => $ra);
+    }
+
+    /**
+     * Create an array of single connection clients.
+     *
+     * @param  array  $servers
+     * @return array
+     */
+    protected function createSingleClients(array $servers) {
+        $clients = array();
+
+        foreach ($servers as $key => $server) {
+        	if ($key === 'cluster') continue;
+
+            $redis = new Redis($server);
+
+            $host    = empty($server['host'])    ? '127.0.0.1' : $server['host'];
+            $port    = empty($server['port'])    ? '6379'      : $server['port'];
+            $timeout = empty($server['timeout']) ? 0           : $server['timeout'];
+
+            if (isset($server['persistent']) && $server['persistent']) {
+            	$redis->pconnect($host, $port, $timeout);
+            } else {
+            	$redis->connect($host, $port, $timeout);
+            }
+
+            if (!empty($server['prefix'])) {
+            	$redis->setOption(Redis::OPT_PREFIX, $server['prefix']);
+            }
+
+            if (!empty($server['database'])) {
+            	$redis->select($server['database']);
+            }
+
+            if (!empty($server['serializer'])) {
+            	$serializer = Redis::SERIALIZER_NONE;
+            	if ($server['serializer'] === 'php') {
+            		$serializer = Redis::SERIALIZER_PHP;
+            	} else if ($server['serializer'] === 'igbinary' && defined('Redis::SERIALIZER_IGBINARY')) {
+            		$serializer = Redis::SERIALIZER_IGBINARY;
+            	}
+            	$redis->setOption(Redis::OPT_SERIALIZER, $serializer);
+            }
+
+            $clients[$key] = $redis;
+        }
+
+        return $clients;
+    }
+}
---
+++ src/Vetruvet/PhpRedis/PhpRedisServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Vetruvet\PhpRedis;
+
+use Vetruvet\PhpRedis\Database;
+
+class PhpRedisServiceProvider extends ServiceProvider {
+	
+	protected $defer = true;
+
+	public function register() {
+		$this->app['redis'] = $this->app->share(function($app) {
+			return new Database($app['config']['database.redis']);
+		});
+	}
+
+	public function provides() {
+		return array('redis');
+	}
+
+}
